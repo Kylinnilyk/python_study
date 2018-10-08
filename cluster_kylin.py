288d5c9d1f12e44004463c32958ae9f51d4f46c7
Kylinnilyk <33738512+Kylinnilyk@users.noreply.github.com>
2018-10-08 17:29:22 +1100

    Update cluster_kylin.py

--- cluster_kylin.py
+++ cluster_kylin.py
@@ -22,7 +22,7 @@ documents = ["Human machine interface for lab abc computer applications",
 stoplist = set('for a of the and to in'.split())
 texts = [[word for word in document.lower().split() if word not in stoplist]
          for document in documents]
-
+pprint(len(texts))
 # remove words that appear only once
 
 frequency = defaultdict(int)
@@ -31,7 +31,21 @@ for text in texts:
   for token in text:
      frequency[token] += 1
 
-texts = [[token for token in text if frequency[token] > 1]
+texts2 = [[token for token in text if frequency[token] > 1]
          for text in texts]
 
-pprint(texts)
+pprint(len(texts2))
+
+dictionary = corpora.Dictionary(texts)
+dictionary.save('/tmp/deerwester.dict')  # store the dictionary, for future reference
+print(dictionary)
+print(dictionary.token2id)
+
+new_doc = "Human computer interaction"
+new_vec = dictionary.doc2bow(new_doc.lower().split())
+print(new_vec)  # the word "interaction" does not appear in the dictionary and is ignored
+
+corpus = [dictionary.doc2bow(text) for text in texts]
+corpora.MmCorpus.serialize('/tmp/deerwester.mm', corpus)  # store to disk, for later use
+print(corpus)
+
